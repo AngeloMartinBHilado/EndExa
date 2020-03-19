ad9e302cc08005f0b171867ef19d706e144daeb4
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-03-19 13:32:42 +0800

    Update config.php

--- google/config.php
+++ google/config.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 $google_client = new Google_Client();
 $google_client->setClientId('889421152360-4nc6sgkra1m5qt4qgb2c05p1km9fdo0m.apps.googleusercontent.com');
 $google_client->setClientSecret('JwgnEhb7emuiA8LAjaFBZkiD');
-$google_client->setRedirectUri('https://endexam.herokuapp.com/index.php');
+$google_client->setRedirectUri('https://endexam.herokuapp.com');
 $google_client->addScope('email');
 $google_client->addScope('profile');
 
