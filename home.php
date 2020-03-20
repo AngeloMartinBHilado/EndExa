8ce59d80199c6d9af8885cb416e239a3224c1cb7
AngeloMartinBHilado <56312511+AngeloMartinBHilado@users.noreply.github.com>
2020-03-20 14:17:26 +0800

    Update home.php

--- home.php
+++ home.php
@@ -3,14 +3,14 @@
 
 
 <div style="background-color: rgba(255,255,255,0.2);
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   margin-bottom: 25px;
   margin-left: auto;
   margin-right: auto;
   width: 50%;">
   <img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:50%">
   <div style="text-align: center;
-  padding: 10px 20px;">
+  padding: 5px 10px;">
   <h1>Profile Picture</h1>
   </div>
 </div>
